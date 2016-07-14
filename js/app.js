d19f9bd2013a3582e1a75b58815aedbbea96daf9
Ryan Bozarth <ryan.bozarth@gmail.com>
2016-07-13 18:32:33 -0700

    small tweaks

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 $( document ).ready(function() {
 
 
-$(".entire-list-item").on('click', function(){ 
+$('.entire-list-item').on('click', function(){ 
     	$(this).find("div.circle").toggleClass("circle-done");
     	$(this).find("div.card-text").toggleClass("text-done");
 });
@@ -10,10 +10,18 @@ $(".entire-list-item").on('click', function(){
   		event.preventDefault();
   		addItem();
 });
+ 
+
 function addItem (){
 	var itemEntered = $("input#textbox").val();
 	$("ul.shopping-list").append("<li class='entire-list-item'><div class='circle'><i class='material-icons'>done</i></div><div class='card-text'><h5>" + itemEntered + "</h5></div></li>");
 };
+
+$(document).keydown(function( event ){
+		if (event.keyCode == 46){
+		$('.circle-done').parent().remove();
+		}
+	});
+
 });
     // end docready
-
