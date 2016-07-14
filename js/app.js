11cf94e27af73db649988ac2777d63f7683ec968
Ryan Bozarth <ryan.bozarth@gmail.com>
2016-07-13 17:00:53 -0700

    ability to cross off items and add new ones

--- js/app.js
+++ js/app.js
@@ -1,19 +1,19 @@
 $( document ).ready(function() {
-    
-    $( ".button-fab" ).click(function() {
-  		$("entire-list-item:last-child").append( "<li></li>" )
+
+
+$(".entire-list-item").on('click', function(){ 
+    	$(this).find("div.circle").toggleClass("circle-done");
+    	$(this).find("div.card-text").toggleClass("text-done");
 });
+  
+    $('form').submit(function() {
+  		event.preventDefault();
+  		addItem();
+});
+function addItem (){
+	var itemEntered = $("input#textbox").val();
+	$("ul.shopping-list").append("<li class='entire-list-item'><div class='circle'><i class='material-icons'>done</i></div><div class='card-text'><h5>" + itemEntered + "</h5></div></li>");
+};
+});
+    // end docready
 
-
-
-
-
-
-
-
-    $(".circle").click(function(){ 
-    	$(this).toggleClass("circle-done");
-    	$(".card-text").toggleClass("text-done");
-});	
-
-})
