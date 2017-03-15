aabb4d33e412f10cd8b286af4a977d696e93b37d
Ryan Bozarth <ryan_bozarth@ryan-bozarth.local>
2017-03-15 15:05:09 -0700

    deleted old files

--- js/app.js
+++ js/app.js
@@ -1,26 +1,27 @@
-$( document ).ready(function() {
+$(document).ready(function() {
 
 
-$('ul.shopping-list').on('click', '.entire-list-item', function(){ 
-    	$(this).find("div.circle").toggleClass("circle-done");
-    	$(this).find("div.card-text").toggleClass("text-done");
-});
-  
+    $('ul.shopping-list').on('click', '.entire-list-item', function() {
+        $(this).find("div.circle").toggleClass("circle-done");
+        $(this).find("div.card-text").toggleClass("text-done");
+    });
+
     $('form').submit(function() {
-  		event.preventDefault();
-  		addItem();
+        event.preventDefault();
+        addItem();
+    });
+
+    function addItem() {
+        var itemEntered = $("input#textbox").val();
+        $("ul.shopping-list").append("<li class='entire-list-item'><div class='circle'><i class='material-icons'>done</i></div><div class='card-text'><h5>" + itemEntered + "</h5></div></li>");
+        $('#textbox').val('');
+    };
+
+    $(document).keydown(function(event) {
+        if (event.keyCode == 9) {
+            $('.circle-done').parent().remove();
+        }
+    });
+
 });
-
-function addItem (){
-	var itemEntered = $("input#textbox").val();
-	$("ul.shopping-list").append("<li class='entire-list-item'><div class='circle'><i class='material-icons'>done</i></div><div class='card-text'><h5>" + itemEntered + "</h5></div></li>");
-};
-
-$(document).keydown(function( event ){
-		if (event.keyCode == 9){
-		$('.circle-done').parent().remove();
-		}
-	});
-
-});
-    // end docready
+// end docready
